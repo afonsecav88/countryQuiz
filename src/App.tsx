f5d9ccbc0d8d2a8498fb751f13f6780c05f96f87
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-08-02 00:15:02 -0300

    create card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Card } from './components/Card';
 import './index.css';
 
 export const App = () => {
-  return <h1 className="text-2xl font-bold">Hello world!</h1>;
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Card />
+    </main>
+  );
 };
---
+++ src/components/Card.tsx
@@ -0,0 +1,8 @@
+export const Card = () => {
+  return (
+    <section className="flex justify-center items-center w-4/5 h-1/2 bg-[#343964] rounded-lg">
+      <p className="font-medium">Country Quiz</p>
+      <p>1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16</p>
+    </section>
+  );
+};
