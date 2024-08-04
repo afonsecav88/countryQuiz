9edc2b0d661ed43c8cf0783e978b2462461ab5c0
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-08-04 12:40:09 -0300

    moved FlawQuestion to AnswerItem component

--- src/components/AnswerItems.tsx
+++ src/components/AnswerItems.tsx
@@ -1,13 +1,17 @@
 import { useCountryContext } from '../hooks/useCountryContext';
 import { usePaginateCountriesAnswer } from '../hooks/usePaginateCountriesAnswer';
+import { FlatQuestion } from './FlatQuestion';
 
 export const AnswerItems = () => {
   const { contriesInfo } = useCountryContext();
   const { questionDataPaginated } = usePaginateCountriesAnswer(contriesInfo);
   console.log(questionDataPaginated);
   return (
-    <div className="flex justify-center items-center gap-8 mt-10">
+    <div className="flex justify-center items-center gap-8 mt-6">
       <div className="flex gap-8 flex-wrap justify-center w-2/3">
+        <h3 className="flex font-medium text-lg text-justify w-96">
+          Which country does this flag {<FlatQuestion />} belong to?
+        </h3>
         {questionDataPaginated.map((answerItems) => (
           <button
             key={answerItems.name.official}
--- src/components/QuizContainer.tsx
+++ src/components/QuizContainer.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { NavQuestion } from './NavQuestion';
-import { FlatQuestion } from './FlatQuestion';
 
 export const QuizContainer = () => {
   return (
     <section className="flex flex-col gap-5 pt-8 items-center w-4/5 h-4/6 min-h-[480px] max-w-7xl bg-[#343964] rounded-lg ">
       <h3 className="flex font-medium text-[#8B8EAB]">Country Quiz</h3>
       <NavQuestion />
-      <h3 className="flex font-medium text-lg text-justify w-96">
-        Which country does this flag {<FlatQuestion />} belong to?
-      </h3>
       <Outlet />
     </section>
   );
