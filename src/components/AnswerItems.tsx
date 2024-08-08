0855d529f7b02b8c02c9456a96039ac98c220faf
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-08-08 20:52:44 -0300

    handle validAnswer state

--- src/components/AnswerItems.tsx
+++ src/components/AnswerItems.tsx
@@ -2,19 +2,26 @@ import { useEffect, useState } from 'react';
 import { useCountryContext } from '../hooks/useCountryContext';
 import { usePaginateCountriesAnswer } from '../hooks/usePaginateCountriesAnswer';
 import { FlatQuestion } from './FlatQuestion';
+import { Countries } from '../Interfaces/CountryInterface';
 
 export const AnswerItems = () => {
   const [clickedButton, setClickedButton] = useState<string>('');
   const [randomIndex, setRandomIndex] = useState(0);
   const { contriesInfo, answerPage } = useCountryContext();
-  const { questionDataPaginated } = usePaginateCountriesAnswer(contriesInfo);
+  const questionDataPaginated = usePaginateCountriesAnswer(contriesInfo);
+  const [validAnswer, seValidAnswer] = useState<Countries[]>(
+    questionDataPaginated.map((prevState) => ({
+      ...prevState,
+      validQuestion: prevState.validQuestion === false,
+    }))
+  );
+  console.log('validAnswer', validAnswer);
 
   //Estado creado para cuando sea respondida la pregunta, mantenga la bandera al navegar
   const randomPositionFlat = () => {
     const randomPosition = Math.floor(
-      Math.random() * questionDataPaginated.length
+      Math.random() * validAnswer.length
     );
-    console.log('answerPage', answerPage);
     setRandomIndex(randomPosition);
   };
 
@@ -22,8 +29,13 @@ export const AnswerItems = () => {
     randomPositionFlat();
   }, [answerPage]);
 
-  const handleClick = (commonName: string) => {
+  const handleClick = (commonName: string, index: number) => {
     setClickedButton(commonName);
+    seValidAnswer((prevState) =>
+      prevState.map((state, i) =>
+        i === index ? { ...state, validQuestion: !state.validQuestion } : state
+      )
+    );
   };
 
   if (questionDataPaginated.length === 0) return;
@@ -38,11 +50,11 @@ export const AnswerItems = () => {
       </h3>
 
       <div className="flex gap-8 flex-wrap justify-center w-2/3">
-        {questionDataPaginated.map((answerItems, index) => (
+        {validAnswer.map((answerItems, index) => (
           <button
             key={answerItems.name.common}
-            disabled={clickedButton !== ''}
-            onClick={() => handleClick(answerItems.name.common)}
+            // disabled={clickedButton !== ''}
+            onClick={() => handleClick(answerItems.name.common, index)}
             className="flex items-center justify-center gap-2 bg-[#393F6F] w-60 h-16 mr-2 p-2 font-medium rounded-2xl text-sm  hover:bg-gradient-to-r from-[#E65895] to-[#BC6BE8]">
             {answerItems.name.common}
             {index === randomIndex && clickedButton !== '' && (
--- src/hooks/usePaginateCountriesAnswer.ts
+++ src/hooks/usePaginateCountriesAnswer.ts
@@ -3,12 +3,10 @@ import { useCountryContext } from './useCountryContext';
 
 export const usePaginateCountriesAnswer = (
   dataCountries: Countries[]
-): { questionDataPaginated: Countries[] } => {
+): Countries[] => {
   const { answerPage, answerPageSize } = useCountryContext();
   const startIndex = (answerPage - 1) * answerPageSize;
   const endIndex = startIndex + answerPageSize;
   const questionDataPaginated = dataCountries.slice(startIndex, endIndex);
-
-  // const resp = questionDataPaginated.sort((x) => Math.random() - 0.5);
-  return { questionDataPaginated };
+  return questionDataPaginated;
 };
