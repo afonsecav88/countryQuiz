93ca1d39a31dc9bfca3815dbf304955ef75c1846
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-08-15 21:37:11 -0300

    fixed router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,30 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { HomeQuiz } from '../HomeQuiz';
-import { AnswerItems } from '../components/AnswerItems';
 import { NotFoundPAge } from '../components/NotFoundPAge';
 import { AnswerCard } from '../components/AnswerCard';
 import { QuizComplete } from '../components/QuizComplete';
 
 export const routes = createBrowserRouter([
   {
-    path: 'quiz',
+    path: 'quiz/question',
     element: <HomeQuiz />,
     errorElement: <NotFoundPAge />,
     children: [
       {
-        path: '/quiz',
+        path: ':questionId',
         element: <AnswerCard />,
-        children: [
-          {
-            path: 'question/:questionId',
-            element: <AnswerItems />,
-          },
-        ],
       },
     ],
   },
   {
     path: 'quiz/completed',
     element: <QuizComplete />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPAge />,
   },
 ]);
