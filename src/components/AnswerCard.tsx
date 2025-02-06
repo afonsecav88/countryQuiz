496aa49b5ca9cfd95cdaff74377f060d842f71f1
Adrian Fonseca Vega <afonsecav88@gmail.com>
2025-02-05 21:10:33 -0300

    adjusted width and height properties in AnswerCard and HomeQuiz components

--- src/components/AnswerCard.tsx
+++ src/components/AnswerCard.tsx
@@ -2,7 +2,7 @@ import { AnswerItems } from './AnswerItems';
 
 export const AnswerCard = () => {
   return (
-    <div className="flex">
+    <div className="flex w-[63vw] min-h-[23vw] justify-center items-center">
       <AnswerItems />
     </div>
   );
